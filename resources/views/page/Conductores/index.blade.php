ac0d45b89248143b5e92bc3ed2f0d7a44d844df2
Guillermo Paredes <a.paredes12345@hotmail.com>
2023-10-18 23:47:04 -0600

    Empezando vistas para cozucoop

---
+++ resources/views/page/Conductores/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-2">
+            <h1 class="text-center">Conductores</h1>
+        </div>
+    </div>
+</div>
+@stop
