c705cc2c350ac554d3987db67537800c8648ff6b
Guillermo Paredes <a.paredes12345@hotmail.com>
2023-10-18 23:47:04 -0600

    Empezando vistas para cozucoop

---
+++ resources/views/page/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-2">
+            <h1 class="text-center">Dashboard</h1>
+        </div>
+    </div>
+    &nbsp;
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Resumen 1</h5>
+                    <p class="card-text">Contenido del resumen 1.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Resumen 2</h5>
+                    <p class="card-text">Contenido del resumen 2.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Resumen 3</h5>
+                    <p class="card-text">Contenido del resumen 3.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
