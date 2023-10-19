29e408aadad2247c1eb3db50a352bfbde8eb3520
Guillermo Paredes <a.paredes12345@hotmail.com>
2023-10-19 13:46:01 -0600

    index de socios

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -93,5 +93,10 @@
 
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/sidebar.js') }}"></script>
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
+  <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+
+  @stack('script')
 </body>
 </html>
