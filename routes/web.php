01a90a087997ccd1a1b6b49db056160f239678a1
Guillermo Paredes <a.paredes12345@hotmail.com>
2023-10-31 12:13:52 -0600

    implementación de login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Procesos\ConductoresController;
 use App\Http\Controllers\Procesos\MovimientosController;
 use App\Http\Controllers\Procesos\ReportesController;
 use App\Http\Controllers\Procesos\SociosController;
 use App\Http\Controllers\Procesos\UsuariosController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,10 +26,14 @@ use Illuminate\Support\Facades\Route;
 // });
 
 //creamos una ruta para el dashboard
+Auth::routes();
+Route::middleware(['auth'])->group(function () {
 Route::resource('/', DashboardController::class)->names('dashboard');
 Route::resource('/conductores', ConductoresController::class)->names('conductores');
 Route::resource('/movimientos', MovimientosController::class)->names('movimientos');
 Route::resource('/reportes', ReportesController::class)->names('reportes');
 Route::resource('/usuarios', UsuariosController::class)->names('usuarios');
 Route::resource('/socios', SociosController::class)->names('socios');
+});
 
+Route::resource('/perfil', ProfileController::class)->names('perfil')->middleware('user', 'fireauth');
